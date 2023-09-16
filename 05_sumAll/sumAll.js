6a72aab4679c1151c5e7b6c236d8360dd43f06c3
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-16 13:20:44 -0400

    sumAll passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number') return 'ERROR';
+    if (min < 0 || min < 0) return 'ERROR';
+    if (min > max) [min, max] = [max, min];
+    let total = 0;
+    for (let i = min; i <= max; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
