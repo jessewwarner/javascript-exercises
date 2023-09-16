35facd4f7822d86baf60ec5e17c4d38bfd30602c
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-16 12:46:03 -0400

    removeFromArray passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArray = arr.filter((value) => { return !args.includes(value)});
+    return newArray;
 };
 
 // Do not edit below this line
