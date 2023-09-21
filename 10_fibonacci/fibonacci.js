f171c8b4eede55b0a85eddecd21ababbc40aa423
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-20 21:06:37 -0400

    Add fibonacci functionality

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
+    if (num <= 2) return 1;
+    if (num > 2){
+        return (fibonacci(num - 2) + fibonacci(num - 1));
+    }
 };
+
+console.log(`Fib: ${fibonacci(7)}`);
 
 // Do not edit below this line
 module.exports = fibonacci;
