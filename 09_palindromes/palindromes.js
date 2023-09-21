2216a544d4eb3c02495c1dd81cf68e6218aefb6e
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-20 20:50:06 -0400

    Add palindromes functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let checkStr = str.toLowerCase();
+    matches = checkStr.match(/[a-zA-Z0-9]+/g);
+    checkStr = matches.join('')
+    let revStr = checkStr.split('').reverse().join('');
+    return checkStr === revStr;
 };
 
 // Do not edit below this line
