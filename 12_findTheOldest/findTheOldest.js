752a41ac54dd250e0375002742c3f5b2dc8e250e
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-20 21:44:28 -0400

    Add findTheOldest functionality

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personObjects) {
+    let oldestPerson = {age: 0, index: 0};
+    const now = new Date().getFullYear();
+    personObjects.forEach((person, index) => {
+        age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : now - person.yearOfBirth;
+        if (oldestPerson.age < age){
+            oldestPerson.age = age;
+            oldestPerson.index = index;
+        }
+    });
+    return personObjects[oldestPerson.index];
 };
 
 // Do not edit below this line
